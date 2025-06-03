6bb4f350cb91c6c36e9c421f0acc3e08ffe3af27
Carlos Faustino <chfcchfc96@gmail.com>
2025-06-03 11:21:41 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/api/:date", async (request, response) => {
+  const { date } = request.params
+
+  const convertedDate = isNaN(date) ? String(date) : Number(date)
+
+  const unix = Math.floor(new Date(convertedDate).getTime())
+
+  if (isNaN(unix)) {
+    return response.json({
+      error: "Invalid Date"
+    })
+  }
+
+  return response.json({
+    unix,
+    utc: new Date(convertedDate).toUTCString()
+  })
+})
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log("HTTP Server running")
+})
